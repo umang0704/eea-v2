aa648356472b4200212a488f0574976053cb768c
Umang Yadav <umang.work82@gmail.com>
2022-02-08 22:29:08 +0530

    080222-0

---
+++ src/test/java/com/eea/v2/V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eea.v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
