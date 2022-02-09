9252c78060359a1be05e77ca9c97409ddb4bab8c
Umang Yadav <umang.work82@gmail.com>
2022-02-09 13:03:59 +0530

    090222-1

--- src/main/java/com/eea/v2/model.pe/PE.java
+++ src/main/java/com/eea/v2/model.pe/PE.java
@@ -1,57 +1,29 @@
 package com.eea.v2.model.pe;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.sql.Date;
 
 @Entity
+@Getter
+@Setter
 public class PE {
     @Id
+    @SequenceGenerator(name = "pe_sequence",sequenceName = "pe_sequence",allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
     private String name;
     private String email;
     private String password;
+
     private String about;
+    private String website;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date date;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAbout() {
-        return about;
-    }
-
-    public void setAbout(String about) {
-        this.about = about;
-    }
+    @Lob
+    private byte[] logo;
 }
