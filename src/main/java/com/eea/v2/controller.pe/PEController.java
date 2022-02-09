d23eb93d7f8caf6ed5eedc84985b0456accd4b8c
Umang Yadav <umang.work82@gmail.com>
2022-02-09 13:03:51 +0530

    090222-1

--- src/main/java/com/eea/v2/controller.pe/PEController.java
+++ src/main/java/com/eea/v2/controller.pe/PEController.java
@@ -14,13 +14,59 @@ public class PEController {
     @Autowired
     private PEService peService;
 
-    @PostMapping(path = "/save")
-    public PE savePE(@RequestBody JSONObject pe){
+    /**
+     * initiate PE account
+     * {
+     *     "name":"-",
+     *     "email":"-",
+     *     "password:"-"
+     * }
+     * Output
+     * {
+     *      "id":"-",
+     *      "name":"-",
+     * }
+     * @param pe
+     * @return
+     */
+    @PostMapping(path = "/initiateAccount")
+    public JSONObject initiateAccount(@RequestBody JSONObject pe){
         PE pe1 =new PE();
         pe1.setName(pe.get("name").toString());
         pe1.setEmail(pe.get("email").toString());
         pe1.setPassword(pe.get("password").toString());
-        return this.peService.savePE(pe1);
+        return this.peService.initiateAccount(pe1);
+    }
+
+    /**
+     * Save Basic Details - saveBasicDetails
+     * Input
+     * {
+     *     "id":"-",
+     *     "about":"-",
+     *     "website-link":"-",
+     *     "from":"-"
+     * }
+     * Output
+     * {
+     *     "id":"-",
+     *     "name":"-",
+     * }
+     * @param entity
+     * @return
+     */
+    @PostMapping(path = "/saveBasicDetails")
+    public JSONObject saveBasicDetails(@RequestBody JSONObject entity){
+        JSONObject ret = new JSONObject();
+        PE pe = this.peService.findById(Integer.parseInt(entity.get("id").toString())).get();
+        if(pe!=null){
+            PE temp = this.peService.saveBasicDetails(pe,entity);
+            ret.put("id",temp.getId());
+            ret.put("name",temp.getName());
+        }else{
+            ret.put("error","Account with id not found");
+        }
+        return ret;
     }
 
     @GetMapping("/all")
